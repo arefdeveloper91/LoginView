fcba56dda7bd12375f6654277bc56a7cdc46b818
aref <devfera14@gmail.com>
2025-09-01 14:48:41 -0300

    Initial Commit

---
+++ LoginView/LoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewApp.swift
+//  LoginView
+//
+//  Created by arefdeveloper on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
